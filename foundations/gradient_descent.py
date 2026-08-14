f1992cb0cdbab2733361c75a0d54c37a17cc96e4
Suhas <95905505+Suhas7842@users.noreply.github.com>
2026-08-14 21:31:51 +0530

    Update Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -4,7 +4,8 @@ class Solution:
         # Derivative:         f'(x) = 2x
         # Update rule:        x = x - learning_rate * f'(x)
         # Round final answer to 5 decimal places
-        x=init
+        if(iterations == 0):
+            return init
         for i in range(iterations):
-            x=x-learning_rate*2*x
-        return round(x, 5)
+            init = init - learning_rate*2*init
+        return round(init, 5)
